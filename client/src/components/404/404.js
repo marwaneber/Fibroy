4937f3b5cca9d3faf183c02b5241192e58019e1d
mac <mac@MacBook-Pro-de-mac-2.local>
2021-08-02 22:49:32 +0100

    setup

---
+++ client/src/components/404/404.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Page404 extends Component {
+  
+
+  render() {
+
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h1>
+                Oups 🧟‍♂️
+            </h1>
+
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem",
+              }}
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Page404;
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://mbDev:Hero123@cluster0-ehjkp.mongodb.net/Fibroy?retryWrites=true&w=majority",
+    secretOrKey: "HakonaMatata"
+};
+  
